34cdf4a41ad38e3a73dc5452b187d4b50c1456e0
arcsinxdx <felixhu0312@gmail.com>
2021-05-25 14:00:15 +0800

    Add files via upload

---
+++ AutoSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Selectable))]
+public class AutoSelect : MonoBehaviour, IPointerEnterHandler {
+
+    [SerializeField] bool autoSelect = true;
+    Selectable selectable;
+
+    private void Awake() {
+        selectable = GetComponent<Selectable>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if(autoSelect)
+            selectable.Select();
+    }
+
+    public void Click() {
+        print("clicked: " + Time.time);
+    }
+
+}
---
+++ EventSystemHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventSystem))]
+public class EventSystemHelper : MonoBehaviour {
+
+    static EventSystem ES;
+    static GameObject cacheObject;
+
+    private void Awake() {
+        ES = GetComponent<EventSystem>();
+    }
+
+    private void Update() {
+
+        // Set cache object
+        if (ES.currentSelectedGameObject != null && ES.currentSelectedGameObject != cacheObject) {
+            cacheObject = ES.currentSelectedGameObject;
+        }
+
+        // Automatically select cache object if nothing is selected
+        else if (cacheObject != null && ES.currentSelectedGameObject == null) {
+            ES.SetSelectedGameObject(cacheObject);
+        }
+
+    }
+
+    // Call from outside to override selected object
+    public static void SetSelectedObject(GameObject go) {
+        if(ES)
+            cacheObject = go;
+    }
+
+}
